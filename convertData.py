589a4035d782144535a5477a30f52d1225e40b0d
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2024-06-04 16:23:42 -0400

    Converting Data

--- convertData.py
+++ convertData.py
@@ -1,9 +1,15 @@
 import csv
 import json
+# Credit: https://www.geeksforgeeks.org/convert-csv-to-json-using-python/
+def csv_to_json(csvFilePath, jsonFilePath):
+    with open(csvFilePath, mode="r", encoding='utf-8') as csv_file:
+        dict_reader = csv.DictReader(csv_file)
+        data = [row for row in dict_reader]
+    with open(jsonFilePath, mode="w", encoding='utf-8') as json_file:
+        json.dumps(data,json_file, indent=4)
 
-social_link_avail= csv.DictReader(open("Social_Links_Avail.csv"))
-for row in social_link_avail_file:
-    print(row)
-link_ep_avail= csv.DictReader(open("Link_Eps_Avail.csv"))
-for row in social_link_avail_file:
-    print(row)
+csv_to_json(Social_Links_Avail.csv, Social_Links_Avail.json)
+
+csv_to_json(Link_Eps_Avail.csv, Link_Eps_Avail.json)
+
+csv_to_json(SL_Unlock_Criteria.csv, SL_Unlock_Criteria.json)
