7188961850c6a153d4dd4dc5ff744d8ea0e51df9
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2024-06-04 16:27:45 -0400

    Changing Unlock Criteria Naming

--- convertData.py
+++ convertData.py
@@ -8,8 +8,8 @@ def csv_to_json(csvFilePath, jsonFilePath):
     with open(jsonFilePath, mode="w", encoding='utf-8') as json_file:
         json.dumps(data,json_file, indent=4)
 
-csv_to_json(Social_Links_Avail.csv, Social_Links_Avail.json)
+csv_to_json(Social_Links_Avail.csv, social_links_avail.json)
 
-csv_to_json(Link_Eps_Avail.csv, Link_Eps_Avail.json)
+csv_to_json(Link_Eps_Avail.csv, link_eps_avail.json)
 
-csv_to_json(SL_Unlock_Criteria.csv, SL_Unlock_Criteria.json)
+csv_to_json(Social_Link_Unlock_Criteria.csv, sl_unlock_criteria.json)
