4c8fda1b1fa66d03a7bfbfd9bfc47f648428f0af
lilymadojemu <70119291+lilymadojemu@users.noreply.github.com>
2024-06-04 16:38:05 -0400

    Finally Created JSON Files

--- convertData.py
+++ convertData.py
@@ -6,10 +6,10 @@ def csv_to_json(csvFilePath, jsonFilePath):
         dict_reader = csv.DictReader(csv_file)
         data = [row for row in dict_reader]
     with open(jsonFilePath, mode="w", encoding='utf-8') as json_file:
-        json.dumps(data,json_file, indent=4)
+        json.dump(data,json_file, indent=4)
 
-csv_to_json(Social_Links_Avail.csv, social_links_avail.json)
+csv_to_json("Social_Links_Avail.csv", "social_links_avail.json")
 
-csv_to_json(Link_Eps_Avail.csv, link_eps_avail.json)
+csv_to_json("Link_Eps_Avail.csv", "link_eps_avail.json")
 
-csv_to_json(Social_Link_Unlock_Criteria.csv, sl_unlock_criteria.json)
+csv_to_json("Social_Link_Unlock_Criteria.csv", "sl_unlock_criteria.json")
